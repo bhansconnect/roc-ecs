d4a18e036d0519b90c9afdca10963f6fee5a3639
Brendan Hansknecht <brendan.hansknecht@gmail.com>
2022-03-30 19:34:20 -0700

    fix build.sh

--- roc-simple-ecs/build.sh
+++ roc-simple-ecs/build.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 set -e
 SCRIPT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
@@ -6,9 +7,9 @@ args="$@"
 cmd="cd ../roc && ./target/release/roc build ../roc-simple-ecs/simpleEcs.roc --no-link --precompiled-host $args"
 
 (cd $SCRIPT_DIR && \
-    ([ -d build ] || meson setup build --buildtype release) && \
-	nix-shell --command "$cmd" ../roc/shell.nix && \
+    (nix-shell --command "$cmd" ../roc/shell.nix && \
 	ar rcs libroc-simple-ecs.a roc-simple-ecs.o && \
+	[ -d build ] || meson setup build --buildtype release) && \
 	ninja -C build)
 
 
